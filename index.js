e735bba6e32a94421620043eabe524ffcdacef55
jasche7 <Jasche@s-169-232-68-76.resnet.ucla.edu>
2017-04-01 22:56:21 -0700

    Removed Justin problems and finished random function

--- index.js
+++ index.js
@@ -1,23 +1,18 @@
 
-function index()
-{
+function index(){
 var mult = 8000 * Math.random() + 1000;
 mult = mult.toFixed(0);
 
-
-//var result = document.getElementById("result");
-//result.innerHTML = mult;
-//result.innerHTML= mult;
-
 var nameToId = {
-	"protein" : 203,
-	"energy" : 208,
+	"Protein" : 203,
+	"Energy" : 208,
+	"Sugars, total" : 269,
+	"Total lipid (fat)" : 204,
+	"Carbohydrate, by difference" : 205
 }
 
-$.get('https://api.nal.usda.gov/ndb/nutrients/?format=json&api_key=DEMO_KEY&nutrients=203&nutrients=204&nutrients=205&nutrients=208&nutrients=269&callback=?&ndbno=01001' , function(data)
-{
+$.get('https://api.nal.usda.gov/ndb/nutrients/?format=json&api_key=DEMO_KEY&nutrients=203&nutrients=204&nutrients=205&nutrients=208&nutrients=269&callback=?&ndbno=0' + mult, function(data){
 	var rtn = {}
-
 	for (var nutrient of data.report.foods[0].nutrients) {
 		for (var name in nameToId) {
 			if (nutrient.nutrient_id == nameToId[name]) {
@@ -28,15 +23,10 @@ $.get('https://api.nal.usda.gov/ndb/nutrients/?format=json&api_key=DEMO_KEY&nutr
 			}
 		}
 	}
-   console.log(rtn);
+	console.log(rtn);
+   return rtn;
    result.innerHTML=rtn[name];
 });
-
-
-
-//$.getJSON('https://api.nal.usda.gov/ndb/nutrients/?format=json&api_key=DEMO_KEY&nutrients=203&nutrients=204&nutrients=205&nutrients=208&nutrients=269&ndbno=0' + mult, function(data){
-//	return data;
-//}
 }
 
 $(document).ready(function() {
