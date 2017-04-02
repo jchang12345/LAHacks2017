d1e54cabe23f4b48e201540dc121e02777e96538
jasche7 <Jasche@s-169-232-68-76.resnet.ucla.edu>
2017-04-01 23:18:00 -0700

    Finished Justin prototype

--- index.js
+++ index.js
@@ -1,9 +1,5 @@
 
-function index(){
-var mult = 8000 * Math.random() + 1000;
-mult = mult.toFixed(0);
-
-var nameToId = {
+let nameToId = {
 	"Protein" : 203,
 	"Energy" : 208,
 	"Sugars, total" : 269,
@@ -11,6 +7,9 @@ var nameToId = {
 	"Carbohydrate, by difference" : 205
 }
 
+function index(){
+var mult = 8000 * Math.random() + 1000;
+mult = mult.toFixed(0);
 $.get('https://api.nal.usda.gov/ndb/nutrients/?format=json&api_key=DEMO_KEY&nutrients=203&nutrients=204&nutrients=205&nutrients=208&nutrients=269&callback=?&ndbno=0' + mult, function(data){
 	var rtn = {}
 	for (var nutrient of data.report.foods[0].nutrients) {
@@ -25,7 +24,6 @@ $.get('https://api.nal.usda.gov/ndb/nutrients/?format=json&api_key=DEMO_KEY&nutr
 	}
 	console.log(rtn);
    return rtn;
-   result.innerHTML=rtn[name];
 });
 }
 
@@ -33,7 +31,7 @@ $(document).ready(function() {
 	$("#searchbox").autocomplete({
 		source: function(request, response) {
 			$.ajax({
-				url: `https://api.nal.usda.gov/ndb/search/?format=json&q=${request.term}&sort=n&max=100&offset=0&api_key=DEMO_KEY`,
+				url: `https://api.nal.usda.gov/ndb/search/?format=json&q=${request.term}&sort=n&max=100&offset=0&api_key=DEMO_KEY&ds=Standard%20Reference`,
 				success: function(data) {
 					console.log(data)
 					var rtn = []
@@ -46,3 +44,30 @@ $(document).ready(function() {
 		}
 	})
 })
+
+function find(){
+	var searchid = document.getElementById("searchbox").value;
+	var ndbno;
+	$.get({'https://api.nal.usda.gov/ndb/search/?format=json&q=' + searchid + '&sort=n&max=100&offset=0&api_key=DEMO_KEY&ds=Standard%20Reference', function(data){
+	for(entry of data.list.item){
+		if(entry.name == searchid){
+			ndbno = entry.ndbno;
+			break;
+		}
+	}}});
+	$.get({'https://api.nal.usda.gov/ndb/nutrients/?format=json&api_key=DEMO_KEY&nutrients=203&nutrients=204&nutrients=205&nutrients=208&nutrients=269&callback=?&ndbno=' + ndbno, function(data){
+	var rtn = {}
+	for (var nutrient of data.report.foods[0].nutrients) {
+		for (var name in nameToId) {
+			if (nutrient.nutrient_id == nameToId[name]) {
+				rtn[name] = {
+					value: nutrient.value,
+					unit: nutrient.unit,
+				}
+			}
+		}
+	}
+	console.log(rtn);
+   return rtn;	})
+});
+}
